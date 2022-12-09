af2805159f154796728430e7c41a803540a72998
Daniel Darnold <danieldarnold@gmail.com>
2022-12-09 00:11:31 -0600

    Added previous year Xmas photos in Accordion

--- src/App.js
+++ src/App.js
@@ -1,79 +1,101 @@
-import React, { useState, useEffect, useCallback } from "react";
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import { fetchPhotos } from './services/FlickrService';
+import { getXmasCards } from "./data/XmasCards";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { PhotoViewer } from "./components/PhotoViewer";
 
 function App() {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-  const [photos, setPhotos] = useState([]);
+  const [flickrPhotos, setFlickrPhotos] = useState([]);
+  const [xmasCards, setXmasCards] = useState([]);
+  const centerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  };
 
   useEffect(() => {
-    const abortController = new AbortController();
     loadPhotos();
-    return () => abortController.abort();
-  },[]);
+  }, []);
 
   const loadPhotos = () => {
     fetchPhotos()
-    .then(response => response.json())
-    .then(data => {
-          const aspectRatio = (height, width) => {
-            if (height > width) {
-                return { width: 1, height: height / width }
-            } else {
-                return { height: 1, width: width / height }
-            }
+      .then(response => response.json())
+      .then(data => {
+        const aspectRatio = (height, width) => {
+          if (height > width) {
+            return { width: 1, height: height / width }
+          } else {
+            return { height: 1, width: width / height }
           }
-          let photos = data.photoset.photo.map(item => {
-            let aspect = aspectRatio(item.height_m, item.width_m);
-            return {
-              src: item.url_l,
-              width: aspect.width,
-              height: aspect.height,
-              title: item.title,
-              alt: item.title,
-              key: item.id,
-              srcSet: [
-                `${item.url_m} ${item.width_m}w`,
-                `${item.url_c} ${item.width_c}w`,
-                `${item.url_l} ${item.width_l}w`,
-                `${item.url_h} ${item.width_h}w`,
-              ],
-              sizes: '(min-width: 480px) 50vw, (min-width: 1024px) 33.3vw, 100vw',
-            };
-          });
-          setPhotos(photos);
+        }
+        let photos = data.photoset.photo.map(item => {
+          let aspect = aspectRatio(item.height_m, item.width_m);
+          return {
+            src: item.url_l,
+            width: aspect.width,
+            height: aspect.height,
+            title: item.title,
+            alt: item.title,
+            key: item.id,
+            srcSet: [
+              `${item.url_m} ${item.width_m}w`,
+              `${item.url_c} ${item.width_c}w`,
+              `${item.url_l} ${item.width_l}w`,
+              `${item.url_h} ${item.width_h}w`,
+            ],
+            sizes: '(min-width: 480px) 50vw, (min-width: 1024px) 33.3vw, 100vw',
+          };
         });
+        
+        setFlickrPhotos(photos);
+        const xmasCards = getXmasCards();
+        setXmasCards(xmasCards);
+      });
   }
-
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
 
   return (
     <div>
-      <Gallery photos={photos} onClick={openLightbox} />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={photos.map(x => ({
-                ...x,
-                srcset: x.srcSet,
-                caption: x.title
-              }))}
-            />
-          </Modal>
-        ) : null}
-      </ModalGateway>
+      <h1 class="greetings" style={centerStyle}>Happy Holidays from the Darnolds!</h1>
+      <div style={centerStyle}>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/E2vKgKif88o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>2022 Gallery</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <PhotoViewer photos={flickrPhotos}></PhotoViewer>
+          </AccordionDetails>
+        </Accordion>
+        {xmasCards.map((card) => {
+          return (
+            <>
+            <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>{card.title}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <PhotoViewer photos={card.cards}></PhotoViewer>
+            </AccordionDetails>
+          </Accordion>
+          </>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/data/XmasCards.js
@@ -0,0 +1,334 @@
+export function getXmasCards() {
+    let cards = [];
+
+    const xmas2021 = {
+        title: '2021',
+        cards: [
+            {
+                src: './static/GroupXmas2021.png',
+                width: 12.36,
+                height: 5.7,
+                title: 'Christmas 2021',
+                srcSet: ['./static/GroupXmas2021.png']
+            }
+        ]
+    };
+    cards.push(xmas2021);
+
+    const xmas2020 = {
+        title: '2020',
+        cards: [
+            {
+                src: './static/Xmas2020.jpg',
+                width: 4.28,
+                height: 5.71,
+                title: 'Christmas 2020',
+                srcSet: ['./static/Xmas2020.jpg']
+            },
+            {
+                src: './static/Xmas2020_.jpg',
+                width: 4.28,
+                height: 5.71,
+                title: 'Christmas 2020',
+                srcSet: ['./static/Xmas2020_.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2020);
+
+    const xmas2019 = {
+        title: '2019',
+        cards: [
+            {
+                src: './static/Xmas2019 (2).jpg',
+                width: 0.6,
+                height: 1.96,
+                title: 'Christmas 2019',
+                srcSet: ['./static/Xmas2019 (2).jpg']
+            }
+        ]
+    };
+    cards.push(xmas2019);
+
+    const xmas2018 = {
+        title: '2018',
+        cards: [
+            {
+                src: './static/Xmas2018.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2018',
+                srcSet: ['./static/Xmas2018.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2018);
+
+    const xmas2017 = {
+        title: '2017',
+        cards: [
+            {
+                src: './static/Xmas2017 (2).jpg',
+                width: 4,
+                height: 1.62,
+                title: 'Christmas 2017',
+                srcSet: ['./static/Xmas2017 (2).jpg']
+            }
+        ]
+    };
+    cards.push(xmas2017);
+
+    const xmas2016 = {
+        title: '2016',
+        cards: [
+            {
+                src: './static/XmasFlip1.jpg',
+                width: 3.69,
+                height: 4.92,
+                title: 'Christmas 2016',
+                srcSet: ['./static/Xmas2017 (2).jpg']
+            },
+            {
+                src: './static/XmasFlip2.jpg',
+                width: 3.69,
+                height: 4.92,
+                title: 'Christmas 2016',
+                srcSet: ['./static/XmasFlip2.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2016);
+
+    const xmas2014 = {
+        title: '2014',
+        cards: [
+            {
+                src: './static/Xmas2014.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2014',
+                srcSet: ['./static/Xmas2014.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2014);
+
+    const xmas2012 = {
+        title: '2012',
+        cards: [
+            {
+                src: './static/Xmas2012.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2012',
+                srcSet: ['./static/Xmas2012.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2012);
+
+    const xmas2011 = {
+        title: '2011',
+        cards: [
+            {
+                src: './static/Xmas2011.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2011',
+                srcSet: ['./static/Xmas2011.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2011);
+
+    const xmas2010 = {
+        title: '2010',
+        cards: [
+            {
+                src: './static/Xmas2010_.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2010',
+                srcSet: ['./static/Xmas2010_.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2010);
+
+    const xmas2009 = {
+        title: '2009',
+        cards: [
+            {
+                src: './static/Xmas2008_1.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2009',
+                srcSet: ['./static/Xmas2008_1.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2009);
+
+    const xmas2008 = {
+        title: '2008',
+        cards: [
+            {
+                src: './static/Xmas2008_2.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2008',
+                srcSet: ['./static/Xmas2008_2.jpg']
+            },
+            {
+                src: './static/Xmas2008_3.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2008',
+                srcSet: ['./static/Xmas2008_3.jpg']
+            },
+            {
+                src: './static/Xmas2008_4.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2008',
+                srcSet: ['./static/Xmas2008_4.jpg']
+            },
+            {
+                src: './static/Xmas2010.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2008',
+                srcSet: ['./static/Xmas2010.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2008);
+
+    const xmas2007 = {
+        title: '2007',
+        cards: [
+            {
+                src: './static/Xmas 2007.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2007',
+                srcSet: ['./static/Xmas 2007.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2007);
+
+    const xmas2006 = {
+        title: '2006',
+        cards: [
+            {
+                src: './static/Xmas2006.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2006',
+                srcSet: ['./static/Xmas2006.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2006);
+
+    const xmas2005 = {
+        title: '2005',
+        cards: [
+            {
+                src: './static/Xmas2005.jpg',
+                width: 3.69,
+                height: 4.92,
+                title: 'Christmas 2005',
+                srcSet: ['./static/Xmas2005.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2005);
+
+    const xmas2004 = {
+        title: '2004',
+        cards: [
+            {
+                src: './static/Xmas2003=4.jpg',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2004',
+                srcSet: ['./static/Xmas2003=4.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2004);
+
+    const xmas2003 = {
+        title: '2003',
+        cards: [
+            {
+                src: './static/XmasArrow2003.jpg',
+                width: 3.69,
+                height: 4.92,
+                title: 'Christmas 2003',
+                srcSet: ['./static/XmasArrow2003.jpg']
+            }
+        ]
+    };
+    cards.push(xmas2003);
+
+    const xmas2002 = {
+        title: '2002',
+        cards: [
+            {
+                src: './static/XmasNaked.JPG',
+                width: 4,
+                height: 3,
+                title: 'Christmas 2002',
+                srcSet: ['./static/XmasNaked.JPG']
+            }
+        ]
+    };
+    cards.push(xmas2002);
+
+    const xmas1999 = {
+        title: '1999',
+        cards: [
+            {
+                src: './static/xmas 1998.jpg',
+                width: 3.69,
+                height: 4.92,
+                title: 'Christmas 1999',
+                srcSet: ['./static/xmas 1998.jpg']
+            }
+        ]
+    };
+    cards.push(xmas1999);
+
+    const xmas1998 = {
+        title: '1998',
+        cards: [
+            {
+                src: './static/Xmas1998.jpg',
+                width: 3.69,
+                height: 4.92,
+                title: 'Christmas 1998',
+                srcSet: ['./static/Xmas1998.jpg']
+            }
+        ]
+    };
+    cards.push(xmas1998);
+
+    const xmas1997 = {
+        title: '1997',
+        cards: [
+            {
+                src: './static/Xmas1997.jpg',
+                width: 3.69,
+                height: 4.92,
+                title: 'Christmas 1997',
+                srcSet: ['./static/Xmas1997.jpg']
+            }
+        ]
+    };
+    cards.push(xmas1997);
+
+    return cards;
+}
